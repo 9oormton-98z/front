20f331e07975f1ea60a994e9565595601aa18f38
Cuzz <rygus9@naver.com>
2022-10-13 01:15:54 +0900

    chore: project environment setting done

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  singleQuote: true,
+  trailingComma: "es5",
+  tabWidth: 2,
+  semi: true,
+  useTabs: false,
+  printWidth: 140,
+  plugins: [require("prettier-plugin-tailwindcss")],
+};
